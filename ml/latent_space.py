d71aad541bad6c9ac4194efbb831531ca05a50a6
bariscankurtkaya <bariscankurtkaya@gmail.com>
2023-08-21 03:43:46 -0400

    latent v2.0 GMM

--- ml/latent_space.py
+++ ml/latent_space.py
@@ -12,13 +12,18 @@ from tqdm import tqdm
 
 class LatentSpace():
     def __init__(self, encoder_dir, data_dir, batch_size, device) -> None:
+        print('Latent Space Class Created.')
         self.device = device
 
         self.encoder = self.__get_encoder(encoder_dir=encoder_dir)
+        print('Encoder Loaded.')
 
+        print('Dataset Loading...')
         dataset = InjectionDataset(data_dir=data_dir)
+        print('Dataset Loaded.')
         dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
+        print('Creating Latent Space...')
         latent_space = self.__create_latent(dataloader)
 
         save_dir = f'{"/".join(encoder_dir.split("/")[:-1])}/latent_v1/train_{encoder_dir.split("/")[-1].split("_")[0]}_latent_space.npy'
@@ -34,7 +39,7 @@ class LatentSpace():
 
     def __create_latent(self, dataloader):
         latent_space = []
-
+        print('Creating Latent Space')
         for _ ,data in enumerate(tqdm(dataloader)):
             image_batch, label = data
             self.encoder.eval()
